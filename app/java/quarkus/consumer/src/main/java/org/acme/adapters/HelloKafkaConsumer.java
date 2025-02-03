73a0355eeed407cb89e4bcb98ec99742efb3ee19
Ricardo J. Nunes <jnunes.ricardo@gmail.com>
2025-02-03 16:45:16 -0300

    core: init consumer and producer configs

---
+++ app/java/quarkus/consumer/src/main/java/org/acme/adapters/HelloKafkaConsumer.java
@@ -0,0 +1,14 @@
+package org.acme.adapters;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class HelloKafkaConsumer {
+
+    @Incoming("hello")
+    public void consume(String message) {
+        // process your price.
+    }
+
+}
---
+++ app/java/quarkus/producer/src/main/java/org/acme/adapters/HelloKafkaProducer.java
@@ -0,0 +1,22 @@
+package org.acme.adapters;
+
+import io.smallrye.mutiny.Multi;
+import java.util.Random;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import java.time.Duration;
+
+@ApplicationScoped
+public class HelloKafkaProducer {
+
+    private final Random random = new Random();
+
+    @Outgoing("hello-out")
+    public Multi<String> generate() {
+        // Build an infinite stream of random strings
+        // It emits a hello string every second
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+            .map(x -> "Hello " + random.nextInt());
+    }
+}
